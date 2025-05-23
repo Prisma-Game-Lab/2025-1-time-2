cd1b68291ead59ba89e173f672723240351167cb
Gabriel Bension Duek <gbduek@gmail.com>
2025-05-23 19:10:33 -0300

    Enemy Collider
    Adicionado colisão ao Enemy e separados em 2 GameObjects distintos os 2 colliders de detecção do player e colisão física

--- ProjetoRobo/Assets/Scripts/Enemies/EnemyController.cs
+++ ProjetoRobo/Assets/Scripts/Enemies/EnemyController.cs
@@ -32,11 +32,19 @@ public class EnemyController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && foundPlayer==false)
         {
             // Handle player trigger
             Debug.Log("Player triggered enemy!");
             foundPlayer = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Handle player trigger
+            Debug.Log("Player collided enemy!");
         }
     }
 }
