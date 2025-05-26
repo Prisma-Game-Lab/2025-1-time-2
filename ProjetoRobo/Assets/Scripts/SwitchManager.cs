8700e64462a09f2dafef8a4589541455b8f47a97
Jesmius <lucas.falvaren@gmail.com>
2025-05-26 19:49:58 -0300

    Added the timer for Input Switch

---
+++ ProjetoRobo/Assets/Scripts/SwitchManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchManager : MonoBehaviour
+{
+    [SerializeField] private float switchTime;
+    [SerializeField] private float warningTime;
+
+    private float switchTimer;
+    private float warningTimer;
+
+    private InputManager inputManager; 
+
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+    if (player != null)
+    {
+        inputManager = player.GetComponent<InputManager>();
+    }
+    else
+    {
+        Debug.LogError("Player not found!");
+    }
+        switchTimer = switchTime;
+        warningTimer = warningTime;
+
+       
+    }
+
+    void Update()
+    {
+        if (inputManager == null) return; // prevent null reference errors
+
+        switchTimer -= Time.deltaTime;
+        print(switchTimer);
+        if (switchTimer <= 0)
+        {
+            warningTimer -= Time.deltaTime;
+            if (warningTimer <= 0)
+            {
+                print(warningTimer);
+                inputManager.SwitchPlayerInput();
+                switchTimer = switchTime;
+                warningTimer = warningTime;
+            }
+        }
+    }
+}
