2b121c5c76e17375e0ce35de5b6b69f5f10dcecf
gbduek <94713966+gbduek@users.noreply.github.com>
2025-06-04 15:09:23 -0300

    merge
    merge

--- ProjetoRobo/Assets/Scripts/Entity/Player/PlayerController.cs
+++ ProjetoRobo/Assets/Scripts/Entity/Player/PlayerController.cs
@@ -9,14 +9,33 @@ public class PlayerController : EntityController
 
     [HideInInspector] public PlayerParry playerParry;
 
+    public bool isAlive = true;
+    public bool isInvincible = false;
+    public int lives = 5; 
+
+    [HideInInspector] public Rigidbody2D rb;
+
     protected override void Awake()
     {
         base.Awake();
-        
+
+        playerParry = GetComponent<PlayerParry>();
         playerMovement = GetComponent<PlayerMovement>();
         inputManager = GetComponent<InputManager>();
 
-        playerParry = GetComponent<PlayerParry>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Laser") && isAlive && !isInvincible)
+        {
+            lives--;
+            Debug.Log(lives + " lives left.");
+            if(lives <= 0){
+                isAlive = false;
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -24,5 +43,3 @@ public class PlayerController : EntityController
          playerParry?.AttemptParry(other.gameObject);
     }
 }
-
-
