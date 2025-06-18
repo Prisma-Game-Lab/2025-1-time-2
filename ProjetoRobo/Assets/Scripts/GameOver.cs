f8045eede0300612a3123ef40a8a5b5a520a1e27
Jesmius <lucas.falvaren@gmail.com>
2025-06-18 01:30:25 -0300

    Fixed minor Game Over Bug

--- ProjetoRobo/Assets/Scripts/GameOver.cs
+++ ProjetoRobo/Assets/Scripts/GameOver.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    public TMP_Text pontos;
 
 
+    public void SetPoints()
+    {
+         UIManager uim = FindObjectOfType<UIManager>();
+        pontos.text = uim.pontos.ToString() + " Pontos";
+    }
     public void Play(string sceneName)
     {
         SceneManager.LoadSceneAsync(sceneName);
