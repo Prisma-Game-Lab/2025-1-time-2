8b7c57bb9318e171b1dca7d9d7811074941cdbc6
Jesmius <lucas.falvaren@gmail.com>
2025-06-29 20:42:39 -0300

    Volume doesnt start at Max Value anymore

--- ProjetoRobo/Assets/Scripts/MainMenu.cs
+++ ProjetoRobo/Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
@@ -8,12 +9,17 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string sceneName;
+
+    [SerializeField] private AudioMixer Mixer;
     [SerializeField] private string menuMusic;
 
     [SerializeField] private string combatMusic;
    
     void Start()
     {
+        Mixer.SetFloat("Volume", Mathf.Log10(0.5f) * 20);
+        Mixer.SetFloat("SFXVolume", Mathf.Log10(0.5f) * 20);
+        Mixer.SetFloat("MusicVolume", Mathf.Log10(0.5f) * 20);
         
         if (AudioManager.Instance != null)
         {
