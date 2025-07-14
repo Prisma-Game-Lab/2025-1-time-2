b98c6ff9a8ce0ec59d6629a1ce55a892c2c20bb4
Iquecs2005 <iquecarvalho2005@gmail.com>
2025-07-14 15:54:12 -0300

    Dialogo com imagens

--- ProjetoRobo/Assets/Scripts/Dialog/DialogController.cs
+++ ProjetoRobo/Assets/Scripts/Dialog/DialogController.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogController : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private GameObject dialogHolder;
     [SerializeField] private TMP_Text textField;
+    [SerializeField] private Image imageField;
 
     [Header("Variables")]
     [SerializeField] private float timeBetweenCharacters;
+    [SerializeField] private Sprite defaultSprite;
 
     private DialogData currentDialogData;
     private int currentDialogIndex;
+    private string currentDialogText;
     private bool onDialog = false;
     private bool writingSentence = false;
     private Coroutine writeSentenceCoroutine;
@@ -28,6 +32,10 @@ public class DialogController : MonoBehaviour
         textField.text = "";
         currentDialogData = dialogData;
         currentDialogIndex = 0;
+        currentDialogText = currentDialogData.dialogueLines[currentDialogIndex].DialogText;
+
+        ChangeSpeakerImage();
+        
         Time.timeScale = 0;
         writeSentenceCoroutine = StartCoroutine(WriteSentence());
     }
@@ -37,26 +45,28 @@ public class DialogController : MonoBehaviour
         if (writingSentence) 
         {
             StopCoroutine(writeSentenceCoroutine);
-            textField.text = currentDialogData.DialogText[currentDialogIndex];
+            textField.text = currentDialogText;
             writingSentence = false;
             return;
         }
 
         currentDialogIndex++;
-        if (currentDialogIndex >= currentDialogData.DialogText.Length) 
+        if (currentDialogIndex >= currentDialogData.dialogueLines.Length)
         {
             EndDialog();
             return;
         }
+        currentDialogText = currentDialogData.dialogueLines[currentDialogIndex].DialogText;
+        ChangeSpeakerImage();
 
         writeSentenceCoroutine = StartCoroutine(WriteSentence());
     }
 
-    private IEnumerator WriteSentence() 
+    private IEnumerator WriteSentence()
     {
         writingSentence = true;
         textField.text = "";
-        foreach (char character in currentDialogData.DialogText[currentDialogIndex]) 
+        foreach (char character in currentDialogText)
         {
             yield return new WaitForSecondsRealtime(timeBetweenCharacters);
             textField.text += character;
@@ -70,5 +80,19 @@ public class DialogController : MonoBehaviour
         textField.text = "";
         Time.timeScale = 1;
         dialogHolder.SetActive(false);
+    }
+
+    private void ChangeSpeakerImage() 
+    {
+        Sprite currentSpeakerSprite = currentDialogData.dialogueLines[currentDialogIndex].SpeakerSprite;
+
+        if (currentSpeakerSprite == null)
+        {
+            imageField.sprite = defaultSprite;
+        }
+        else
+        {
+            imageField.sprite = currentSpeakerSprite;
+        }
     }
 }
--- ProjetoRobo/Assets/Scripts/Dialog/DialogData.cs
+++ ProjetoRobo/Assets/Scripts/Dialog/DialogData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DialogData", menuName = "ScriptableObjects/DialogData")]
 public class DialogData : ScriptableObject
 {
+    //[TextArea]
+    //public string[] DialogText;
+
+    public SpeechData[] dialogueLines;
+}
+
+[Serializable]
+public class SpeechData
+{
     [TextArea]
-    public string[] DialogText;
+    public string DialogText;
+    [SerializeField]
+    public Sprite SpeakerSprite; 
 }
