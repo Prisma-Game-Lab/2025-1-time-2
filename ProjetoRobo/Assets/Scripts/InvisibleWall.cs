3a9ec987b025b42a8529d53787906c158ddbe9ac
Jesmius <lucas.falvaren@gmail.com>
2025-07-16 16:19:46 -0300

    Changed the Tutorial Map, Added Dialogs when colliding with the invisible wall + Fixed minor UI Bug in EntitiesTest

--- ProjetoRobo/Assets/Scripts/InvisibleWall.cs
+++ ProjetoRobo/Assets/Scripts/InvisibleWall.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InvisibleWall : MonoBehaviour
 {
     [Header("Number of Enemies that Need to Die")]
     [SerializeField] public int enemiesNeeded;
 
+    public UnityEvent OnDestroy;
 
    
     void Update()
     {
-        
+
         if (enemiesNeeded <= 0)
         {
+            OnDestroy?.Invoke();
             Destroy(gameObject);
+            
         }
     }
 
