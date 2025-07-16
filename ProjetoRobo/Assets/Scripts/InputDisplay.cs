8681eb5e77513557836e0786e6905bd00b4d7a25
Jesmius <lucas.falvaren@gmail.com>
2025-07-15 21:06:08 -0300

    Added Button Sprites to the Controls + Added New Fonts

--- ProjetoRobo/Assets/Scripts/InputDisplay.cs
+++ ProjetoRobo/Assets/Scripts/InputDisplay.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 using System.ComponentModel;
+using UnityEngine.UI;
+using UnityEngine.InputSystem.Android;
 
 public class InputDisplay : MonoBehaviour
 {
@@ -12,15 +14,37 @@ public class InputDisplay : MonoBehaviour
 
     [Header("UI Text Fields")]
 
-    public TMP_Text P1Text;
-    public TMP_Text P2Text;
+    [SerializeField] private GameObject RebindText;
 
-    public TMP_Text P1Switch;
-    public TMP_Text P2Switch;
+    [SerializeField] private TMP_Text P1Text;
+    [SerializeField] private TMP_Text P2Text;
 
-    public TMP_Text[] P1Movement = new TMP_Text[4];
+    [SerializeField] private TMP_Text P1Switch;
+    [SerializeField] private TMP_Text P2Switch;
 
-    public TMP_Text[] P2Movement = new TMP_Text[4];
+    [SerializeField] private TMP_Text[] P1Movement = new TMP_Text[4];
+
+    [SerializeField] private TMP_Text[] P2Movement = new TMP_Text[4];
+
+    [Header("P1Buttons")]
+    [SerializeField] private Button P1Button;
+
+    [SerializeField] private Button P1Morph;
+    [SerializeField] private Button[] P1MovementButtons = new Button[4];
+
+    [Header("P2Buttons")]
+
+    [SerializeField] private Button P2Button;
+
+    [SerializeField] private Button P2Morph;
+
+    [SerializeField] private Button[] P2MovementButtons = new Button[4];
+
+    [Header("Buttons Sprites")]
+
+    [SerializeField] private Sprite smallKey;
+
+    [SerializeField] private Sprite longKey;
 
     string[] movement = { "Up: ", "Down: ", "Left: ", "Right: " };
 
@@ -32,20 +56,20 @@ public class InputDisplay : MonoBehaviour
 
     void Start()
     {
-
+       
         //   .PerformInteractiveRebinding(bindingIndex)
-        GetSingleBindingDisplay("ButtonP1", P1Text);
-        GetSingleBindingDisplay("ButtonP2", P2Text);
+        GetSingleBindingDisplay("ButtonP1", P1Text,P1Button);
+        GetSingleBindingDisplay("ButtonP2", P2Text,P2Button);
 
-        GetSingleBindingDisplay("ChangeActionP1", P1Switch);
-        GetSingleBindingDisplay("ChangeActionP2", P2Switch);
+        GetSingleBindingDisplay("ChangeActionP1", P1Switch,P1Morph);
+        GetSingleBindingDisplay("ChangeActionP2", P2Switch,P2Morph);
 
-        GetBindingDisplay("AxisP1", P1Movement, movement);
-        GetBindingDisplay("AxisP2", P2Movement, movement);
+        GetBindingDisplay("AxisP1", P1Movement, movement,P1MovementButtons);
+        GetBindingDisplay("AxisP2", P2Movement, movement,P2MovementButtons);
 
 
     }
-    void GetSingleBindingDisplay(string actionName, TMP_Text textElement)
+    void GetSingleBindingDisplay(string actionName, TMP_Text textElement, Button button)
     {
         var action = inputActions.FindAction(actionName);
         if (action == null)
@@ -62,10 +86,21 @@ public class InputDisplay : MonoBehaviour
                 InputControlPath.HumanReadableStringOptions.OmitDevice
             );
             textElement.text = displayName;
+            if (displayName.Length == 1)
+            {
+                button.image.sprite = smallKey;
+                button.image.SetNativeSize();
+            }
+            else
+            {
+
+                button.image.sprite = longKey;
+                button.image.SetNativeSize();
+            }
         }
     }
 
-    void GetBindingDisplay(string actionPath, TMP_Text[] movement, string[] keybind)
+    void GetBindingDisplay(string actionPath, TMP_Text[] movement, string[] keybind, Button[] buttons)
     {
         var action = inputActions.FindAction(actionPath);
 
@@ -93,13 +128,24 @@ public class InputDisplay : MonoBehaviour
                     );
 
                     movement[j - 1].text = displayName;
+                    if (displayName.Length == 1)
+                    {
+                    buttons[j-1].image.sprite = smallKey;
+                    buttons[j-1].image.SetNativeSize();
+                    }
+                    else
+                    {
+
+                    buttons[j-1].image.sprite = longKey;
+                    buttons[j-1].image.SetNativeSize();
+                    }
 
                 }
                 break;
             }
         }
     }
-    public void StartRebinding(string actionMapName, string actionName, TMP_Text text)
+    public void StartRebinding(string actionMapName, string actionName, TMP_Text text, Button button)
     {
         var actionMap = inputActions.FindActionMap(actionMapName);
         if (actionMap == null)
@@ -118,35 +164,39 @@ public class InputDisplay : MonoBehaviour
 
 
         currentAction.Disable();
-        text.text = "Press Any Key";
+        RebindText.SetActive(true);
+        text.text = "?";
+        
         currentAction
             .PerformInteractiveRebinding(0)
             .WithCancelingThrough("<Keyboard>/escape")
             .OnComplete(operation =>
             {
                 currentAction.Enable();
-                GetSingleBindingDisplay(actionName, text);
+                RebindText.SetActive(false);
+                GetSingleBindingDisplay(actionName, text, button);
                 operation.Dispose();
 
             })
             .Start();
+        
     }
     public void RebindButtonP1()
     {
-        StartRebinding("Normal", "ButtonP1", P1Text);
+        StartRebinding("Normal", "ButtonP1", P1Text,P1Button);
     }
     public void RebindButtonP2()
     {
-        StartRebinding("Normal", "ButtonP2", P2Text);
+        StartRebinding("Normal", "ButtonP2", P2Text,P2Button);
     }
 
     public void RebindSwitchP1()
     {
-        StartRebinding("Normal", "ChangeActionP1", P1Switch);
+        StartRebinding("Normal", "ChangeActionP1", P1Switch,P1Morph);
     }
      public void RebindSwitchP2()
     {
-        StartRebinding("Normal", "ChangeActionP2", P2Switch);
+        StartRebinding("Normal", "ChangeActionP2", P2Switch,P2Morph);
     }
     int GetDirectionIndex(string direction)
     {
@@ -159,7 +209,7 @@ public class InputDisplay : MonoBehaviour
             default: return -1;
         }
     }
-    public void StartRebindingMovement(string actionName, string compositePart, TMP_Text[] displayTextArray)
+    public void StartRebindingMovement(string actionName, string compositePart, TMP_Text[] displayTextArray, Button[] buttons)
     {
 
         var action = inputActions.FindAction(actionName);
@@ -190,11 +240,13 @@ public class InputDisplay : MonoBehaviour
         }
         displayText = displayTextArray[GetDirectionIndex(compositePart.ToLower())];
         action.actionMap.Disable();
-        displayText.text = "Press Any Key";
+        displayText.text = "?";
+        RebindText.SetActive(true);
         action.PerformInteractiveRebinding(bindingIndex)
             .WithCancelingThrough("<Keyboard>/escape")
             .OnComplete(operation =>
             {
+                RebindText.SetActive(false);
                 string newBinding = InputControlPath.ToHumanReadableString(
                 action.bindings[bindingIndex].effectivePath,
                 InputControlPath.HumanReadableStringOptions.OmitDevice
@@ -202,18 +254,30 @@ public class InputDisplay : MonoBehaviour
 
                 if (displayText != null)
                     displayText.text = $"{newBinding}";
+                if (newBinding.Length == 1)
+                {
+                    buttons[GetDirectionIndex(compositePart.ToLower())].image.sprite = smallKey;
+                    buttons[GetDirectionIndex(compositePart.ToLower())].image.SetNativeSize();
+                }
+                else
+                {
+
+                    buttons[GetDirectionIndex(compositePart.ToLower())].image.sprite = longKey;
+                    buttons[GetDirectionIndex(compositePart.ToLower())].image.SetNativeSize();
+                    }
 
                 operation.Dispose();
                 action.actionMap.Enable();
             })
         .Start();
+        
     }
     public void RebindMovementP1(string direction)
     {
-        StartRebindingMovement("AxisP1", direction, P1Movement);
+        StartRebindingMovement("AxisP1", direction, P1Movement,P1MovementButtons);
     }
     public void RebindMovementP2(string direction)
     {
-        StartRebindingMovement("AxisP2",direction,P2Movement);
+        StartRebindingMovement("AxisP2",direction,P2Movement,P2MovementButtons);
     }
 }
